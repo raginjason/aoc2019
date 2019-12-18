ed6bc28b4e128d2b4ae5ea1293d56dc74ce0854e
Jason Walker <ragin.jason@me.com>
2019-12-18 00:26:02 -0500

    Converted the rest of day 5 tests

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -8,6 +8,120 @@ import (
 	"strings"
 )
 
+type ParameterMode int
+
+const (
+	PositionMode ParameterMode = iota
+	ImmediateMode
+)
+
+func (m ParameterMode) String() string {
+	if m < PositionMode || m > ImmediateMode {
+		return "Unknown"
+	}
+
+	return [...]string{"Position", "Immediate"}[m]
+}
+
+type OpCode int
+
+const (
+	Add OpCode = iota + 1
+	Multiply
+	Input
+	Output
+	JumpIfTrue
+	JumpIfFalse
+	LessThan
+	Equals
+	Terminate OpCode = 99
+)
+
+func (op OpCode) String() string {
+	if op < Add || op > Terminate {
+		return "Unknown"
+	}
+
+	if op == Terminate {
+		return "Terminate"
+	}
+
+	names := [...]string{
+		"N/A",
+		"Add",
+		"Multiply",
+		"Input",
+		"Output",
+		"JumpIfTrue",
+		"JumpIfFalse",
+		"LessThan",
+		"Equals",
+	}
+
+	return names[op]
+}
+
+type Instruction struct {
+	OpCode         OpCode
+	ParameterModes []ParameterMode
+}
+
+func NewInstruction(i int) *Instruction {
+	ins := new(Instruction)
+
+	str := strconv.Itoa(i)
+	letters := strings.Split(str, "")
+
+	var opStr string
+
+	if len(letters) > 1 {
+		opStr = letters[len(letters)-2] + letters[len(letters)-1]
+		letters = letters[:len(letters)-2] // Remove last 2 items
+	} else if len(letters) == 1 {
+		opStr = letters[len(letters)-1]
+		letters = letters[:len(letters)-1] // Remove last item
+	}
+
+	// Setup the OpCode
+	if val, err := strconv.Atoi(opStr); err == nil {
+		ins.OpCode = OpCode(val)
+	} else {
+		log.Fatalf("could not convert %s%s to int OpCode: %s", letters[len(letters)-2], letters[len(letters)-1], err)
+	}
+
+	var parms int
+	switch ins.OpCode {
+	case Add, Multiply:
+		parms = 3
+	case Input, Output:
+		parms = 1
+	case JumpIfTrue, JumpIfFalse:
+		parms = 2
+	case LessThan, Equals:
+		parms = 3
+	default:
+		parms = 0
+	}
+
+	ins.ParameterModes = make([]ParameterMode, parms)
+
+	// Reverse in place
+	for i := len(letters)/2 - 1; i >= 0; i-- {
+		opp := len(letters) - 1 - i
+		letters[i], letters[opp] = letters[opp], letters[i]
+	}
+
+	for i := range letters {
+		if val, err := strconv.Atoi(letters[i]); err == nil {
+			ins.ParameterModes[i] = ParameterMode(val)
+		} else {
+			log.Fatalf("could not convert value %s to int ParameterMode[%d]: %s", letters[i], i, err)
+		}
+	}
+
+	return ins
+}
+
 func (p Program) String() string {
 	var strs []string
 	for _, num := range p {
@@ -36,6 +150,20 @@ func NewComputer(input chan int, program []int, output chan int) *Computer {
 	return c
 }
 
+func splitInt(i int) []int {
+	str := strconv.Itoa(i)
+	letters := strings.Split(str, "")
+	ints := []int{}
+	for _, i := range letters {
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatalf("could not convert %s to int", i)
+		}
+		ints = append(ints, j)
+	}
+	return ints
+}
+
 func (c *Computer) Run() {
 	inputCounter := 0
 	for i := 0; i < len(c.Program); {
@@ -50,53 +178,63 @@ func (c *Computer) Run() {
 			break
 		}
 
-		opCodeParts := splitInt(op)
-
-		switch opCodeParts[len(opCodeParts)-1] {
-		case 1: // Add
+		ins := NewInstruction(op)
+		switch ins.OpCode {
+		case Add:
 			var input1Value int
 			var input2Value int
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				input1Value = c.Program[i+1] // Immediate mode
-			} else {
-				input1Value = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				input1Value = c.Program[i+1]
+			case PositionMode:
+				input1Value = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				input2Value = c.Program[i+2] // Immediate mode
-			} else {
-				input2Value = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				input2Value = c.Program[i+2]
+			case PositionMode:
+				input2Value = c.Program[c.Program[i+2]]
 			}
 
 			outputAddress := c.Program[i+3]
 			c.Program[outputAddress] = input1Value + input2Value
 			i = i + 4
-		case 2: // Multiply
+		case Multiply:
 			var input1Value int
 			var input2Value int
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				input1Value = c.Program[i+1] // Immediate mode
-			} else {
-				input1Value = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				input1Value = c.Program[i+1]
+			case PositionMode:
+				input1Value = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				input2Value = c.Program[i+2] // Immediate mode
-			} else {
-				input2Value = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				input2Value = c.Program[i+2]
+			case PositionMode:
+				input2Value = c.Program[c.Program[i+2]]
 			}
 
 			outputAddress := c.Program[i+3]
 			c.Program[outputAddress] = input1Value * input2Value
 			i = i + 4
-		case 3:
+		case Input:
 			/*
 			 * Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For
 			 * example, the instruction 3,50 would take an input value and store it at address 50.
 			 */
-			outputAddress := c.Program[i+1]
+			var outputAddress int
+
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				outputAddress = c.Program[c.Program[i+1]]
+			case PositionMode: // zeroval
+				outputAddress = c.Program[i+1]
+			}
 
 			if val, ok := <-c.input; ok {
 				c.Program[outputAddress] = val
@@ -104,22 +242,23 @@ func (c *Computer) Run() {
 
 			inputCounter = inputCounter + 1
 			i = i + 2
-		case 4:
+		case Output:
 			/*
 			 * Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the
 			 * value at address 50.
 			 */
 			var outputValue int
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				outputValue = c.Program[i+1] // Immediate mode
-			} else {
-				outputValue = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				outputValue = c.Program[i+1]
+			case PositionMode:
+				outputValue = c.Program[c.Program[i+1]]
 			}
 
 			c.output <- outputValue
 			i = i + 2
-		case 5: // Jump-if-true
+		case JumpIfTrue:
 			/*
 			 * If the first parameter is non-zero, it sets the instruction pointer to the value from the second
 			 * parameter. Otherwise, it does nothing.
@@ -127,16 +266,18 @@ func (c *Computer) Run() {
 			var flag int
 			var newPointer int
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				flag = c.Program[i+1] // Immediate mode
-			} else {
-				flag = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				flag = c.Program[i+1]
+			case PositionMode:
+				flag = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				newPointer = c.Program[i+2] // Immediate mode
-			} else {
-				newPointer = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				newPointer = c.Program[i+2]
+			case PositionMode:
+				newPointer = c.Program[c.Program[i+2]]
 			}
 
 			if flag != 0 {
@@ -144,7 +285,7 @@ func (c *Computer) Run() {
 			} else {
 				i = i + 3
 			}
-		case 6: // Jump-if-false
+		case JumpIfFalse:
 			/*
 			 * If the first parameter is zero, it sets the instruction pointer to the value from the second parameter.
 			 * Otherwise, it does nothing.
@@ -152,16 +293,18 @@ func (c *Computer) Run() {
 			var flag int
 			var newPointer int
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				flag = c.Program[i+1] // Immediate mode
-			} else {
-				flag = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				flag = c.Program[i+1]
+			case PositionMode:
+				flag = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				newPointer = c.Program[i+2] // Immediate mode
-			} else {
-				newPointer = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				newPointer = c.Program[i+2]
+			case PositionMode:
+				newPointer = c.Program[c.Program[i+2]]
 			}
 
 			if flag == 0 {
@@ -169,7 +312,7 @@ func (c *Computer) Run() {
 			} else {
 				i = i + 3
 			}
-		case 7: // Less than
+		case LessThan:
 			/*
 			 * If the first parameter is less than the second parameter, it stores 1 in the position given by the third
 			 * parameter. Otherwise, it stores 0.
@@ -179,16 +322,18 @@ func (c *Computer) Run() {
 			var input2Value int
 			outputAddress := c.Program[i+3]
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				input1Value = c.Program[i+1] // Immediate mode
-			} else {
-				input1Value = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				input1Value = c.Program[i+1]
+			case PositionMode:
+				input1Value = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				input2Value = c.Program[i+2] // Immediate mode
-			} else {
-				input2Value = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				input2Value = c.Program[i+2]
+			case PositionMode:
+				input2Value = c.Program[c.Program[i+2]]
 			}
 
 			if input1Value < input2Value {
@@ -197,7 +342,7 @@ func (c *Computer) Run() {
 				c.Program[outputAddress] = 0
 			}
 			i = i + 4
-		case 8:
+		case Equals:
 			/*
 			 * Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position
 			 * given by the third parameter. Otherwise, it stores 0.
@@ -207,16 +352,18 @@ func (c *Computer) Run() {
 			var input2Value int
 			outputAddress := c.Program[i+3]
 
-			if len(opCodeParts)-3 >= 0 && opCodeParts[len(opCodeParts)-3] == 1 {
-				input1Value = c.Program[i+1] // Immediate mode
-			} else {
-				input1Value = c.Program[c.Program[i+1]] // Position mode
+			switch ins.ParameterModes[0] {
+			case ImmediateMode:
+				input1Value = c.Program[i+1]
+			case PositionMode:
+				input1Value = c.Program[c.Program[i+1]]
 			}
 
-			if len(opCodeParts)-4 >= 0 && opCodeParts[len(opCodeParts)-4] == 1 {
-				input2Value = c.Program[i+2] // Immediate mode
-			} else {
-				input2Value = c.Program[c.Program[i+2]] // Position mode
+			switch ins.ParameterModes[1] {
+			case ImmediateMode:
+				input2Value = c.Program[i+2]
+			case PositionMode:
+				input2Value = c.Program[c.Program[i+2]]
 			}
 
 			if input1Value == input2Value {
@@ -230,20 +377,6 @@ func (c *Computer) Run() {
 		}
 		c.InstructionPointer = i
 	}
-}
-
-func splitInt(i int) []int {
-	str := strconv.Itoa(i)
-	letters := strings.Split(str, "")
-	ints := []int{}
-	for _, i := range letters {
-		j, err := strconv.Atoi(i)
-		if err != nil {
-			log.Fatalf("could not convert %s to int", i)
-		}
-		ints = append(ints, j)
-	}
-	return ints
 }
 
 func ParseIntCode(reader io.Reader) Program {
--- intcode/intcode_test.go
+++ intcode/intcode_test.go
@@ -2,6 +2,7 @@ package intcode
 
 import (
 	"github.com/google/go-cmp/cmp"
+	"strconv"
 	"testing"
 )
 
@@ -270,6 +271,192 @@ func TestInputOutput(t *testing.T) {
 
 			if diff := cmp.Diff(tc.wantInstructionPointer, c.InstructionPointer); diff != "" {
 				t.Errorf("Instruction pointer difference, -want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestInstruction(t *testing.T) {
+	testCases := []struct {
+		input int
+		want  *Instruction
+	}{
+		{99, &Instruction{Terminate, []ParameterMode{}}},
+
+		{1, &Instruction{Add, []ParameterMode{PositionMode, PositionMode, PositionMode}}},
+		{101, &Instruction{Add, []ParameterMode{ImmediateMode, PositionMode, PositionMode}}},
+		{1001, &Instruction{Add, []ParameterMode{PositionMode, ImmediateMode, PositionMode}}},
+		{1101, &Instruction{Add, []ParameterMode{ImmediateMode, ImmediateMode, PositionMode}}},
+		{10001, &Instruction{Add, []ParameterMode{PositionMode, PositionMode, ImmediateMode}}},
+		{10101, &Instruction{Add, []ParameterMode{ImmediateMode, PositionMode, ImmediateMode}}},
+		{11001, &Instruction{Add, []ParameterMode{PositionMode, ImmediateMode, ImmediateMode}}},
+		{11101, &Instruction{Add, []ParameterMode{ImmediateMode, ImmediateMode, ImmediateMode}}},
+
+		{2, &Instruction{Multiply, []ParameterMode{PositionMode, PositionMode, PositionMode}}},
+		{102, &Instruction{Multiply, []ParameterMode{ImmediateMode, PositionMode, PositionMode}}},
+		{1002, &Instruction{Multiply, []ParameterMode{PositionMode, ImmediateMode, PositionMode}}},
+		{1102, &Instruction{Multiply, []ParameterMode{ImmediateMode, ImmediateMode, PositionMode}}},
+		{10002, &Instruction{Multiply, []ParameterMode{PositionMode, PositionMode, ImmediateMode}}},
+		{10102, &Instruction{Multiply, []ParameterMode{ImmediateMode, PositionMode, ImmediateMode}}},
+		{11002, &Instruction{Multiply, []ParameterMode{PositionMode, ImmediateMode, ImmediateMode}}},
+		{11102, &Instruction{Multiply, []ParameterMode{ImmediateMode, ImmediateMode, ImmediateMode}}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(strconv.Itoa(tc.input), func(t *testing.T) {
+			ins := NewInstruction(tc.input)
+
+			if diff := cmp.Diff(tc.want, ins); diff != "" {
+				t.Errorf("Instruction difference, -want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestJump(t *testing.T) {
+	bigProgram := Program{
+		3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99,
+	}
+	testCases := []struct {
+		inputData  []int
+		program    Program
+		wantOutput []int
+	}{
+		// Output 0 if input was 0 else output 1, mode=position
+		{[]int{-1}, Program{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, []int{1}},
+		{[]int{0}, Program{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, []int{0}},
+		{[]int{1}, Program{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, []int{1}},
+		{[]int{2}, Program{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, []int{1}},
+
+		// Output 0 if input was 0 else output 1, mode=immediate
+		{[]int{-1}, Program{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, []int{1}},
+		{[]int{0}, Program{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, []int{0}},
+		{[]int{1}, Program{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, []int{1}},
+		{[]int{2}, Program{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, []int{1}},
+
+		/*
+		 * bigProgram uses an input instruction to ask for a single number. The program will then output 999 if the
+		 * input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is
+		 * greater than 8.
+		 */
+		{[]int{7}, bigProgram, []int{999}},
+		{[]int{8}, bigProgram, []int{1000}},
+		{[]int{9}, bigProgram, []int{1001}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.program.String(), func(t *testing.T) {
+			in := make(chan int)
+			out := make(chan int)
+			c := NewComputer(in, tc.program, out)
+			go c.Run()
+
+			for _, v := range tc.inputData {
+				in <- v
+			}
+
+			var got []int
+			for {
+				val, ok := <-out
+				if ok == false {
+					break
+				} else {
+					got = append(got, val)
+				}
+			}
+
+			if diff := cmp.Diff(tc.wantOutput, got); diff != "" {
+				t.Errorf("Output difference, -want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestEquals(t *testing.T) {
+	testCases := []struct {
+		testName   string
+		inputData  []int
+		program    Program
+		wantOutput []int
+	}{
+		{"position-mode test input 7 equal to 8", []int{7}, Program{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, []int{0}},
+		{"position-mode test input 8 equal to 8", []int{8}, Program{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, []int{1}},
+		{"position-mode test input 9 equal to 8", []int{9}, Program{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, []int{0}},
+
+		{"immediate-mode test input 7 equal to 8", []int{7}, Program{3, 3, 1108, -1, 8, 3, 4, 3, 99}, []int{0}},
+		{"immediate-mode test input 8 equal to 8", []int{8}, Program{3, 3, 1108, -1, 8, 3, 4, 3, 99}, []int{1}},
+		{"immediate-mode test input 9 equal to 8", []int{9}, Program{3, 3, 1108, -1, 8, 3, 4, 3, 99}, []int{0}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			in := make(chan int)
+			out := make(chan int)
+			c := NewComputer(in, tc.program, out)
+			go c.Run()
+
+			for _, v := range tc.inputData {
+				in <- v
+			}
+
+			var got []int
+			for {
+				val, ok := <-out
+				if ok == false {
+					break
+				} else {
+					got = append(got, val)
+				}
+			}
+
+			if diff := cmp.Diff(tc.wantOutput, got); diff != "" {
+				t.Errorf("Output difference, -want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestLessThan(t *testing.T) {
+	testCases := []struct {
+		testName   string
+		inputData  []int
+		program    Program
+		wantOutput []int
+	}{
+		{"position-mode test input 7 less than 8", []int{7}, Program{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, []int{1}},
+		{"position-mode test input 8 less than 8", []int{8}, Program{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, []int{0}},
+		{"position-mode test input 9 less than 8", []int{9}, Program{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, []int{0}},
+
+		{"immediate-mode test input 7 less than 8", []int{7}, Program{3, 3, 1107, -1, 8, 3, 4, 3, 99}, []int{1}},
+		{"immediate-mode test input 8 less than 8", []int{8}, Program{3, 3, 1107, -1, 8, 3, 4, 3, 99}, []int{0}},
+		{"immediate-mode test input 9 less than 8", []int{9}, Program{3, 3, 1107, -1, 8, 3, 4, 3, 99}, []int{0}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			in := make(chan int)
+			out := make(chan int)
+			c := NewComputer(in, tc.program, out)
+			go c.Run()
+
+			for _, v := range tc.inputData {
+				in <- v
+			}
+
+			var got []int
+			for {
+				val, ok := <-out
+				if ok == false {
+					break
+				} else {
+					got = append(got, val)
+				}
+			}
+
+			if diff := cmp.Diff(tc.wantOutput, got); diff != "" {
+				t.Errorf("Output difference, -want +got:\n%s", diff)
 			}
 		})
 	}
