0ff8a70def8d7f351b18118db265ce4af4a14d47
Jason Walker <ragin.jason@me.com>
2019-12-18 21:42:04 -0500

    Day 9 part 1 solved

---
+++ day9/day9.go
@@ -0,0 +1,42 @@
+package day9
+
+import (
+	"github.com/raginjason/aoc2019/intcode"
+	"github.com/raginjason/aoc2019/util"
+	"log"
+	"os"
+)
+
+func scanDay9File() []int {
+	fh, err := os.Open(util.InputFilePath(9))
+	if err != nil {
+		log.Fatalf("could not open file: %s", err)
+	}
+	defer fh.Close()
+
+	program := intcode.ParseIntCode(fh)
+
+	return program
+}
+
+func Part1() int {
+	program := scanDay9File()
+
+	in := make(chan int)
+	out := make(chan int)
+	c := intcode.NewComputer(in, program, out)
+	go c.Run()
+	in <- 1
+
+	var output []int
+	for {
+		val, ok := <-out
+		if !ok {
+			break
+		} else {
+			output = append(output, val)
+		}
+	}
+	log.Printf("output: %v", output)
+	return output[0]
+}
